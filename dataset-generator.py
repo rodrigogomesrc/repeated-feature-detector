c3bb16172cadd976627e76022dade3db6a08cecf
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2022-12-14 17:49:43 -0300

    fix grid and other adjustments

--- dataset-generator.py
+++ dataset-generator.py
@@ -14,21 +14,21 @@ def resize_image(image, resolution):
     return cv2.resize(image, (resolution, resolution))
 
 
-def create_non_matching_images(images_path, quantity, grid_size):
+def create_non_matching_images(images_path, quantity, grid_size, resolution):
     random_indices = get_n_random_indices(quantity, len(images_path), repeat=False)
-    images = [get_resized_image(images_path[random_index], 64) for random_index in random_indices]
-    return create_images_collage(images, grid_size, 64)
+    images = [get_resized_image(images_path[random_index], resolution) for random_index in random_indices]
+    return create_images_collage(images, grid_size, resolution)
 
 
-def create_matching_images(images_path, quantity, grid_size):
+def create_matching_images(images_path, quantity, grid_size, resolution):
     random_indices = get_random_indices_with_one_repetition(quantity, len(images_path))
-    images = [get_resized_image(images_path[random_index], 64) for random_index in random_indices]
-    return create_images_collage(images, grid_size, 64)
+    images = [get_resized_image(images_path[random_index], resolution) for random_index in random_indices]
+    return create_images_collage(images, grid_size, resolution)
 
 
 def get_random_indices_with_one_repetition(quantity, max_value):
-    random_indices = get_n_random_indices(quantity,max_value, repeat=True)
-    repeated_indice = random.randint(0, len(random_indices) - 1)
+    random_indices = get_n_random_indices(quantity -1, max_value, repeat=True)
+    repeated_indice = random.randint(0, quantity -1)
     repeated_number = random_indices[repeated_indice]
     random_indices.append(repeated_number)
     return random_indices
@@ -41,14 +41,10 @@ def create_images_collage(images, grid_size, resolution):
     while len(images) < grid_size * grid_size:
         images.append(get_black_image(resolution))
 
+    collage = np.zeros((grid_size * resolution, grid_size * resolution, 3), dtype=np.uint8)
     for i in range(grid_size):
-        image_line = images[0]
-        for j in range(1, grid_size):
-            image_line = np.concatenate((image_line, images[i * grid_size + j]), axis=1)
-        if i == 0:
-            collage = image_line
-        else:
-            collage = np.concatenate((collage, image_line), axis=0)
+        for j in range(grid_size):
+            collage[i * resolution:(i + 1) * resolution, j * resolution:(j + 1) * resolution, :] = images[i * grid_size + j]
 
     return collage
 
@@ -80,25 +76,26 @@ def save_image(image, folder, image_name):
     cv2.imwrite(path, image)
 
 
-def create_collages(quantity, images_path, grid_size):
+def create_collages(quantity, images_path, grid_size, resolution):
     images_path_list = get_images_file_paths(images_path)
     for i in range(quantity):
-        non_matching_image = create_non_matching_images(images_path_list, grid_size * grid_size, grid_size)
-        matching_image = create_matching_images(images_path_list, grid_size * grid_size, grid_size)
+        non_matching_image = create_non_matching_images(images_path_list, grid_size * grid_size, grid_size, resolution)
+        matching_image = create_matching_images(images_path_list, grid_size * grid_size, grid_size, resolution)
         save_image(non_matching_image, "generated-dataset/non-matching", "non-matching-{}.jpg".format(i))
         save_image(matching_image, "generated-dataset/matching", "matching-{}.jpg".format(i))
       
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python dataset-generator.py <entry-images-path>")
+    if len(sys.argv) != 5:
+        print("Usage: python dataset-generator.py <images_folder_path> <quantity> <grid_size> <resolution>")
         exit(1)
 
     images_folder_path = sys.argv[1]
     quantity = int(sys.argv[2])
     grid_size = int(sys.argv[3])
+    resolution = int(sys.argv[4])
 
-    create_collages(quantity, images_folder_path, grid_size)
+    create_collages(quantity, images_folder_path, grid_size, resolution)
     
     
